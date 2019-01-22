75bca8e558f186646ce5e9e5fc92ba6b0ee05b1b
Conal <k00209777@student.lit.ie>
2019-01-22 10:47:16 +0000

    Console.log Tests

---
+++ untitled1/Test.js
@@ -0,0 +1,27 @@
+//console.log(234 == '234'); True
+
+//console.log(234 === '234'); False
+
+//console.log(234234.235235 == 'cat');
+
+//console.log("cat" == "CAT");
+
+//console.log("cat".toLocaleUpperCase() == "CAT");
+
+//console.log(typeof "cat");
+
+//console.log(8*8);
+
+//console.log(parseInt("234.12345"));
+
+//console.log(parseInt("cat"));
+
+//console.log(isNaN(parseInt("cat")));
+
+//var i = "this is a test".indexOf("is");
+//console.log(i);
+
+var s = "this is a test string.".substr(19,3);
+var s1 = "this is a test string.".slice(19,22);
+console.log(s);
+console.log(s1);
